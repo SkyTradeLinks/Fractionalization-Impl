105ce0c1cfb44ac8bdb08cea4e067f11653df976
Peter Munachi <alimuanchipeter@gmail.com>
2025-06-14 17:42:39 +0200

    fix

--- contract-hardhat/contracts/mocks/FunctionSigClash1.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT 
-pragma solidity 0.8.30;
-
-contract functionSigClash1 {
-    // function clash550254402() public {
-    // }
-}
--- contract-hardhat/contracts/mocks/FunctionSigClash2.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT 
-pragma solidity 0.8.30;
-
-contract functionSigClash2 {
-    // function proxyOwner() public {
-    // }
-}
