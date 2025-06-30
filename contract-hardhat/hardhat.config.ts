722cdee0bd5ec4bccf22194cb296de0c6765cbe0
Peter Munachi <alimuanchipeter@gmail.com>
2025-06-30 19:53:15 +0200

    fix: coverage fix

--- contract-hardhat/hardhat.config.ts
+++ contract-hardhat/hardhat.config.ts
@@ -2,16 +2,20 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
+  mocha: {
+    timeout: 900000,
+  },
   solidity: {
     compilers: [
       {
         version: "0.8.30",
         settings: {
-        optimizer: {
-          enabled: true,
-          runs: 200,
-        },
-        viaIR: true,
+          optimizer: {
+            enabled: true,
+            runs: 200,
+            details: { yul: false },
+          },
+          viaIR: true,
         },
       },
     ],
